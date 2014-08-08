4c6b1c8891e877d7d2f1b8988d7318d17f91fb4d
Taras Demianchenko <demtarico@gmail.com>
2014-08-08 09:59:10 +0300

    Initial Commit

---
+++ app/code/community/SaM/Bestsellers/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taric
+ * Date: 8/8/14
+ * Time: 9:32 AM
+ */ 
+class Sam_Bestsellers_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/community/SaM/Bestsellers/sql/sam_bestsellers_setup/install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taric
+ * Date: 8/8/14
+ * Time: 9:32 AM
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
